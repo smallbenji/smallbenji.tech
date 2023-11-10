5b6cfaef6f4210a11a20746de50be1a513f24448
smallbenji <benjaminkfalch@gmail.com>
2023-11-10 20:37:45 +0100

    yes

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <>
+      <div className="Frame">
+        <div className="left">
+          <h1>smallbenji.tech</h1>
+          <h2>Programmør</h2>
+        </div>
+        <div className="right">
+          <img src="./smallbenji.jpg" alt="" />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
